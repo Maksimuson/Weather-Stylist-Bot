c21c14a5cebf3cec26240495b6c44b0ae9d8e38b
Максим Шмаков <shagmax2019@gmail.com>
2026-09-18 20:00:36 +0300

    add AI

--- src/hendlers.py
+++ src/hendlers.py
@@ -1,14 +1,16 @@
 import os
+import asyncio
 from aiogram import Router, types, F
 from aiogram.filters import CommandStart
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from geopy.geocoders import Nominatim
 from geopy.adapters import AioHTTPAdapter
-from dotenv import load_dotenv, load, load_dotenvdotenv
+from dotenv import load_dotenv
 from google import genai
 
 
 load_dotenv()
+client= genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 router = Router()
 
@@ -45,5 +47,13 @@ async def location_handler(message: types.Message):
 
     await message.answer(f"Your city is: {city}. Now, let's find the perfect outfit for you based on the weather in {city}!")
 
-    # response = client.models.generate_content()
+    response = await asyncio.to_thread(
+    client.models.generate_content,
+    model="gemini-3.6-flash",
+    contents=f"Please provide a stylish outfit recommendation for the current weather in {city}"
 
+    )
+
+    await message.answer(response.text)
+    
+    print("KEY LOADED:", bool(os.getenv("GEMINI_API_KEY")))
